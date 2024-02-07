232ccc349387529fa787ae14013225e93eb31bf9
Laband <lukeabandeen@gmail.com>
2024-02-07 06:39:32 -0500

    updated description and css

--- script.js
+++ script.js
@@ -16,15 +16,17 @@ let titleArr = [];
 let og = "";
 
 button.addEventListener("click", search);
-button2.addEventListener("click",display2);
+button2.addEventListener("click", display2);
 
+search();
 
 function search(e) {
-	e.preventDefault();
+	e?.preventDefault();
 	wrap = 0;
 	wrap2 = 9;
 	//console.log(input.value);
 	let title = input.value;
+	input.value = "";
 	const words = title.split(" ");
 	let title2 = words.join("+");
 	
@@ -35,6 +37,22 @@ function search(e) {
 	})
 	.then(res=> {
 		titleArr.push(res.docs[0].title);
+		fetch(`${url3}${res.docs[0].key}.json`)
+		.then(res => {
+			return res.json();
+		})
+		.then(res=> {
+			let z = document.querySelector(".main");
+			if(res?.description?.value) {
+				z.innerText = res.description.value; 
+			}
+			else if(res?.description) {
+				z.innerText = res.description;
+			}
+			else {
+				z.innerText = "This book has no blurb on file, but I do hope you have a most wonderful day!"
+			}
+		})
 		og = res.docs[0].title;
 		//console.log(res.docs[0].cover_i);
 		id = res.docs[0].cover_i;
@@ -92,16 +110,29 @@ function display(res) {
 				y = resp?.description?.value;	
 				if(y){
 					const mockHTML = `
+					<div class="rec">
 						<h3>${res.docs[i].title}</h3>
 						<img src="${url2}id/${res.docs[i].cover_i}-M.jpg" alt="rec book">
-						<p>${y}</p>`
+						<p>${y}</p>
+					</div>`
+					x.insertAdjacentHTML("beforeend", mockHTML);
+				}
+				else if(resp?.description) {
+					const mockHTML = `
+					<div class = "rec">
+						<h3>${res.docs[i].title}</h3>
+						<img src="${url2}id/${res.docs[i].cover_i}-M.jpg" alt="rec book">
+						<p>${resp.description}</p>
+					</div>`
 					x.insertAdjacentHTML("beforeend", mockHTML);
 				}
 				else {
 					const mockHTML = `
-					<h3>${res.docs[i].title}</h3>
-					<img src="${url2}id/${res.docs[i].cover_i}-M.jpg" alt="rec book">
-					<p>This book does not have a blurb</p>`
+					<div class="rec">
+						<h3>${res.docs[i].title}</h3>
+						<img src="${url2}id/${res.docs[i].cover_i}-M.jpg" alt="rec book">
+						<p>This book does not have a blurb, and thus I have decided to add some text here purely for the sake of asthetics. So now that I have your attention, what shall we discuss? Anthing in mind? Oooo, good point. We should in fact talk about how great of a website this is. How smart of you.</p>
+					</div>`
 					x.insertAdjacentHTML("beforeend", mockHTML);
 				}
 			})
@@ -119,6 +150,5 @@ function display2(e) {
 	e.preventDefault();
 	display(books);
 }
-
 
 //console.log("sup2");
